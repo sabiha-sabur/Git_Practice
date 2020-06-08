57ff19e32f54278c3d6c0ec9c7bee113c68bd09e
sabiha-sabur <sabiha.sabur@gmail.com>
2020-06-08 16:01:13 +0100

    Create practice method

---
+++ Tutorial.py
@@ -0,0 +1,38 @@
+print("Hello, World!")
+
+if 5>2:
+    print( "Five is greater than two!")
+
+x = 5
+y= "Hello, World!"
+
+print( x, y)
+# The above is just practice
+#This is a comment 
+#written in
+#more than just one line
+print("You can not seen my comments within the code")
+
+#To create a multi-line string you can use 3 speech marks
+
+"""
+This is a
+long list of text
+which is an example of a multi string line
+"""
+print("I have just created a multi-line of string text that acts as a comment")
+
+#working on variables
+x = 4 # x is of type int -otherwise known as integer 
+y = "Sabiha" #is of type str- otherwise known as string
+
+z = "Siyana"
+# the variables cna be quoted with double or single quotes, so the below is the same
+z = 'Siyana'
+
+#Let's discover variable names
+
+print(x)
+print(y)
+
+exit()
